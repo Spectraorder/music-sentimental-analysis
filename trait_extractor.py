36ecd9ae8977d713c666012fd177afdd376438b6
Spectraorder <2421036333@qq.com>
2023-12-22 00:51:55 -0500

    dataset | musical traits combined

--- trait_extractor.py
+++ trait_extractor.py
@@ -1,6 +1,7 @@
 import librosa
 import numpy as np
 import pandas as pd
+import os
 
 
 def extract_musical_traits(filename):
@@ -24,35 +25,65 @@ def extract_musical_traits(filename):
     zero_crossing_rate = librosa.feature.zero_crossing_rate(y)[0]
 
     # Format features as strings
-    tempo_str = f"{tempo:.1f} BPM"
+    tempo_str = f"{tempo:.1f}"
     spectral_centroid_value = spectral_centroid[0]  # Access the first value
-    spectral_centroid_str = f"{spectral_centroid_value:.1f} Hz"
+    spectral_centroid_str = f"{spectral_centroid_value:.1f}"
     spectral_bandwidth_value = spectral_bandwidth[0]  # Access the first value
-    spectral_bandwidth_str = f"{spectral_bandwidth_value:.1f} Hz"
+    spectral_bandwidth_str = f"{spectral_bandwidth_value:.1f}"
     chroma_stft_str = str(np.mean(chroma_stft_mean))
     zero_crossing_rate_value = zero_crossing_rate[0]  # Access the first value
-    zero_crossing_rate_str = f"{zero_crossing_rate_value:.1%}"
+    zero_crossing_rate_str = f"{zero_crossing_rate_value:.1}"
 
     # Create a CSV file
-    data = {
+    traits = {
         "Tempo": tempo_str,
         "Spectral Centroid": spectral_centroid_str,
         "Spectral Bandwidth": spectral_bandwidth_str,
         "Chroma Features": chroma_stft_str,
         "Zero Crossing Rate": zero_crossing_rate_str
-        # "Loudness": loudness_str
     }
-    print("Extracted Musical Traits:")
-    print(f"Tempo: {tempo_str}")
-    print(f"Spectral Centroid: {spectral_centroid_str}")
-    print(f"Spectral Bandwidth: {spectral_bandwidth_str}")
-    print(f"Chroma Features: {chroma_stft_str}")
-    print(f"Zero Crossing Rate: {zero_crossing_rate_str}")
 
-    df = pd.DataFrame(data, index=[0])
-    df.to_csv("musical_traits.csv", index=False)
+    return traits
+    # print("Extracted Musical Traits:")
+    # print(f"Tempo: {tempo_str} BPM")
+    # print(f"Spectral Centroid: {spectral_centroid_str} Hz")
+    # print(f"Spectral Bandwidth: {spectral_bandwidth_str} Hz")
+    # print(f"Chroma Features: {chroma_stft_str}")
+    # print(f"Zero Crossing Rate: {zero_crossing_rate_str}%")
+    #
+    # df = pd.DataFrame(data, index=[0])
+    # df.to_csv("musical_traits.csv", index=False)
+
+
+def process_audio_files(folder_path):
+    """Processes multiple audio files in a folder and saves the results to a CSV file.
+
+    Args:
+        folder_path (str): Path to the folder containing the audio files.
+
+    Returns:
+        None
+    """
+    # Get a list of all files in the folder
+    files = [f for f in os.listdir(folder_path) if f.endswith('.wav')]
+
+    # Create an empty DataFrame to store the results
+    result_df = pd.DataFrame()
+
+    # Process each audio file and append the results to the DataFrame
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        traits = extract_musical_traits(file_path)
+        result_df = result_df.append(traits, ignore_index=True)
+        print(f"Processing file: {file}")
+
+    # Save the combined results to a CSV file
+    result_df.to_csv("musical_traits.csv", index=False)
+
+    print("Combined Musical Traits:")
+    print(result_df)
 
 
 # Example usage
-filename = '../Dataset/blues.00000.wav'
-extract_musical_traits(filename)
+folder_path = '../Dataset/'
+process_audio_files(folder_path)
